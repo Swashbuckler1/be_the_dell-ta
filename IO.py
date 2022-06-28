b09cc39ee130014a52f1c39279be240d0a3a8cdb
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2022-06-28 12:08:33 -0400

    Saving

--- IO.py
+++ IO.py
@@ -1,15 +1,10 @@
 import pandas as pd
 
-def read_file(sheet_url):
-    url = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
-    df = pd.read_csv(url)
-    return df
-
 def open_file(file):
     with open(file) as f:
         terms = f.read().splitlines()
     return terms
 
-# url = 'https://docs.google.com/spreadsheets/d/1GxATEQYgmFyvCQbt3ZXQretIFU8an36aeGxx2PFwoZU/edit#gid=0'
-# df = read_file(url)
-# print(df.head())
+file = 'https://raw.githubusercontent.com/Swashbuckler1/be_the_dell-ta/master/Dell_Interns_Directory.csv'
+df = pd.read_csv(file)
+print(df['TechnologiesOfInterest'].values[1])
